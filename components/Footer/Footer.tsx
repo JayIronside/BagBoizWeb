eb571a4125bafdb319365a1b4ffa5243154318e9
JayIronside <151539687+JayIronside@users.noreply.github.com>
2024-12-03 16:36:18 +0100

    Delete components directory

--- components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css';
-
-const Footer = () => (
-  <footer className="footer">
-    <p>&copy; 2024 Bag Boiz Empire. All Rights Reserved.</p>
-  </footer>
-);
-
-export default Footer;
